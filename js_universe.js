92aecf57c87b599b1af4bf4ab5ab59f3f228714f
LisaCee <lisaceedesign@gmail.com>
2020-02-22 10:53:02 -0800

    day 2

---
+++ js_universe.js
@@ -0,0 +1,28 @@
+// primitive values
+console.log(2);
+console.log("hello");
+console.log(undefined);
+
+// objects and functions
+console.log({});
+console.log([]);
+console.log(x => x * 2);
+
+// expressions = questions that JS can answer
+console.log(2 + 2);
+
+// types
+console.log(typeof(2));
+console.log(typeof("hello"));
+console.log(typeof(undefined));
+
+console.log(typeof({}));
+console.log(typeof([]));
+console.log(typeof(x => x * 2));
+
+// without parens
+console.log(typeof {});
+console.log(typeof []);
+//console.log(typeof x => x * 2);
+// this one breaks without parens
+
